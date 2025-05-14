34d998ade792a2c098bd0ceaa3ac90d2d0edbfd2
Fanderz <135126601+Fanderz@users.noreply.github.com>
2025-05-14 14:14:31 +0300

    feat: creating constructors to some Presenters
    - Add constructor to PalletPresenter.
    - Add constructor to CrystallBagPresenter.

--- Assets/ParrotCargo/Scripts/MVP/Presenters/CrystallBagPresenter.cs
+++ Assets/ParrotCargo/Scripts/MVP/Presenters/CrystallBagPresenter.cs
@@ -1,3 +1,11 @@
 public class CrystallBagPresenter
 {
+    private BaseCrystallBagView _view;
+    private BaseCrystallBag _crystallBagModel;
+
+    public CrystallBagPresenter(BaseCrystallBagView view, BaseCrystallBag model)
+    {
+        _view = view;
+        _crystallBagModel = model;
+    }
 }
--- Assets/ParrotCargo/Scripts/MVP/Presenters/PalletPresenter.cs
+++ Assets/ParrotCargo/Scripts/MVP/Presenters/PalletPresenter.cs
@@ -1,3 +1,11 @@
 public class PalletPresenter
 {
+    private PalletView _view;
+    private Pallet _palletModel;
+
+    public PalletPresenter(PalletView view, Pallet model)
+    {
+        _view = view;
+        _palletModel = model;
+    }
 }

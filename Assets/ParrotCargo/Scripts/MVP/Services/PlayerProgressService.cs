e4fdc39c9e6a036ae0a30e82d4e2650bada1924f
Fanderz <135126601+Fanderz@users.noreply.github.com>
2025-11-18 17:06:28 +0300

    fix
    - small fixes after import new 3d model of bag

--- Assets/ParrotCargo/Scripts/MVP/Services/PlayerProgressService.cs
+++ Assets/ParrotCargo/Scripts/MVP/Services/PlayerProgressService.cs
@@ -18,10 +18,7 @@ public class PlayerProgressService : BaseService
     public override void Initialize()
     {
         _coinsPresenter = new CoinsPresenter(YG2.saves.coinsProgress, _coinsView, _smoothIncreaseWait);
-        //_coinsPresenter.Initialize();
-
         _pointsPresenter = new PointsPresenter(YG2.saves.pointsProgress, _pointsView, _smoothIncreaseWait);
-        //_pointsPresenter.Initialize();
     }
 
     public void IncreaseValuesOnBagRelease()

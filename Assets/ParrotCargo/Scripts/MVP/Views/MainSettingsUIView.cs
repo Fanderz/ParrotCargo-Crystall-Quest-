c1ffebfb0a88ad98498c4629b6e3bde7e95e1ad4
VladRadov <picvladislav@yandex.ru>
2025-11-21 00:02:41 +0300

    fix: fixed logic active settings ui

---
+++ Assets/ParrotCargo/Scripts/MVP/Views/MainSettingsUIView.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainSettingsUIView : MonoBehaviour
+{
+    [SerializeField] private Button _buttonSettingsOpen;
+    [SerializeField] private Button _buttonSettingsClose;
+    [SerializeField] private Button _buttonGoHome;
+
+    public void ChangeActive(bool isInGame)
+    {
+        ChangeActiveButtons(isInGame);
+
+        gameObject.SetActive(!gameObject.activeSelf);
+    }
+
+    private void ChangeActiveButtons(bool isInGame)
+    {
+        _buttonSettingsClose.gameObject.SetActive(!isInGame);
+        _buttonGoHome.gameObject.SetActive(isInGame);
+    }
+}

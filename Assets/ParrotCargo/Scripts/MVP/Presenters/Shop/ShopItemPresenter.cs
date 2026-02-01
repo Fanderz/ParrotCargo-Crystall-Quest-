670f082869b338d26ba6d15381d7f0838c0172c5
Fanderz <135126601+Fanderz@users.noreply.github.com>
2026-02-02 00:56:14 +0300

    fix: optimization size build

--- Assets/ParrotCargo/Scripts/MVP/Presenters/Shop/ShopItemPresenter.cs
+++ Assets/ParrotCargo/Scripts/MVP/Presenters/Shop/ShopItemPresenter.cs
@@ -2,7 +2,6 @@ using System.Collections.Generic;
 using System.Linq;
 using UniRx;
 using UnityEngine;
-using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class ShopItemPresenter
 {
